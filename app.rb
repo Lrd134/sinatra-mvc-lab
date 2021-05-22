3cf9a678019dde2b0d5d9df7b9806a95f8cd0446
Lrd134 <larryc3200@gmail.com>
2021-05-21 20:29:39 -0400

    commit tree 0a51ae3dbb5bed695d32897db39c1185b0c4700f

--- app.rb
+++ app.rb
@@ -4,10 +4,7 @@ class App < Sinatra::Base
     get "/" do
         erb :"/user_input"
     end
-    get "/piglatinize" do
-        @phrases = PigLatinizer.all
-        erb :"/index"
-    end
+
     post "/piglatinize" do
         @pl = PigLatinizer.new
         @phrase = @pl.piglatinize(params["user_phrase"])
